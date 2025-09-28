26a8a12221e31fce0f067247407b5dcd6c99f8b7
Divin2510 <divinvinith@gmail.com>
2025-09-29 00:50:40 +0530

    done

--- src/main/java/org/demo/Sample.java
+++ src/main/java/org/demo/Sample.java
@@ -1,5 +1,25 @@
 package org.demo;
 
 public class Sample {
+	
+	private void empName() {
+		System.out.println("Name is Divin");
+
+	}
+	
+	private void empId() {
+		System.out.println("ID is 45");
+
+	}
+	
+	public static void main(String[] args) {
+		
+		Sample s = new Sample();
+		s.empName();
+		s.empId();
+		
+		
+		
+	}
 
 }
