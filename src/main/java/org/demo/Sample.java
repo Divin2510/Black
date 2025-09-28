065ed62176c21c35621c71ac47404ea68665807c
Divin2510 <divinvinith@gmail.com>
2025-09-29 01:15:57 +0530

    ok

--- src/main/java/org/demo/Sample.java
+++ src/main/java/org/demo/Sample.java
@@ -1,5 +1,21 @@
 package org.demo;
 
 public class Sample {
+	
+	private void studName() {
+		System.out.println("Name is divin");
+
+	}
+	
+	private void studRank() {
+		System.out.println("Rank is 8");
+
+	}
+	
+	public static void main(String[] args) {
+		Sample a = new Sample();
+		a.studName();
+		a.studRank();
+	}
 
 }
